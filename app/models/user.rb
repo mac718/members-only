1e6cdebd0d44486d6b1e5c7258d08ab2a997d089
mac718 <mac718@gmail.com>
2018-03-03 15:17:37 -0500

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
+  before_save { email.downcase }
   before_create :remember
+  
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false },  
+            format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -21,5 +31,5 @@ class User < ApplicationRecord
     Bcrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
-  has_secure_password
+  
 end
