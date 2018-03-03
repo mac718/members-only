c2c08fe44a31e0024a13b04e6cc060cc4a85918d
mac718 <mac718@gmail.com>
2018-03-03 15:08:46 -0500

    Set up sessions, sign in, and sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      sign_in(@user)
+      redirect_to 'new'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,13 @@ class User < ApplicationRecord
   end
 
   def remember
-    self.remember_digest = User.generate_digest(User.new_remember_token)
+    self.remember_token = User.new_remember_token
+    self.remember_digest = User.generate_digest(remember_token)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_token.nil?
+    Bcrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   has_secure_password
